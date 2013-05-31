0904f54b7c631183db4e088512f6e25fe18113b8
gurken <tbscho@gmx.de>
2013-05-31 14:53:08 +0200

    rails new create

---
+++ sprinkler_v1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SprinklerV1::Application.config.secret_key_base = 'eded1e3e761447e12258dc0355b56f87f3aacff5700444d5604c90edb520b780a34b5bb7429adbdc639c00aa75a44d3de7f4ce8337c468985de09b05ed837027'
---
+++ sprinkler_v1/vendor/bundle/gems/json-1.8.0/ext/json/ext/parser/siteconf20130531-65087-1f21lv2.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130531-65087-1idka0r"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
